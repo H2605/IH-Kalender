7c4c92d3a4225ebb7e4b88caedf9732f45f2c7bc
H2605 <hhoaduc@yahoo.com>
2025-01-31 11:52:53 +0100

    Upload Files

---
+++ flask_calender/app.py
@@ -0,0 +1,33 @@
+import json
+
+#mport streamlit as st
+#from streamlit_calendar import calendar
+from flask import jsonify
+from flask import Flask
+from flask import request
+from flask import render_template
+
+data="/Users/huyduc/Documents/flask_calender/sources/allevents_4.json"
+#filename="/Users/huyduc/Downloads/dhvi_calendar_code/batch_data.json"
+with open(data, 'r', encoding='utf-8') as json_file:
+    data = json.load(json_file)
+app = Flask(__name__)
+events = data#[
+#    {"title": "Meeting", "start": "2025-02-05"},
+#    {"title": "Geburtstag", "start": "2025-02-12"},
+#]
+
+@app.route('/')
+def index():
+    return render_template('kalender.html')
+                           #/Users/huyduc/Downloads/dhvi_calendar_code/dash-full-calendar/assets/kalender.html')
+
+@app.route('/events')
+def get_events():
+    return jsonify(events)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+render_
