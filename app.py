7a8a7f343a5a054828b52f6b73c440ad1bd50fd5
H2605 <hhoaduc@yahoo.com>
2025-02-08 17:25:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 import json
-#import streamlit as st
+
+#mport streamlit as st
 #from streamlit_calendar import calendar
-#from flask import Flask, render_template, jsonify
-import os.path
 from flask import jsonify
 from flask import Flask
 from flask import request
 from flask import render_template
+import os.path
 
 my_path = os.path.abspath(os.path.dirname(__file__))
-app = Flask(__name__, template_folder='templates')
 data = os.path.join(my_path, "./sources/allevents_4.json")
-#app = Flask(__name__)
 
+#data="../sources/allevents_4.json"
+#filename="/Users/huyduc/Downloads/dhvi_calendar_code/batch_data.json"
 with open(data, 'r', encoding='utf-8') as json_file:
     data = json.load(json_file)
+app = Flask(__name__)
 events = data#[
 #    {"title": "Meeting", "start": "2025-02-05"},
 #    {"title": "Geburtstag", "start": "2025-02-12"},
@@ -22,7 +23,7 @@ events = data#[
 
 @app.route('/')
 def index():
-    return render_template('kalendar.html')
+    return render_template('kalender.html')
                            #/Users/huyduc/Downloads/dhvi_calendar_code/dash-full-calendar/assets/kalender.html')
 
 @app.route('/events')
@@ -31,3 +32,6 @@ def get_events():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+#render_
