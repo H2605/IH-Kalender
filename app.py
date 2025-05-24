4f0ea18bfe425c15e76f22ef9b4099c0bc48f060
H2605 <hhoaduc@yahoo.com>
2025-05-24 15:20:32 +0200

    Code Cleaning

--- app.py
+++ app.py
@@ -10,26 +10,16 @@ import os.path
 
 my_path = os.path.abspath(os.path.dirname(__file__))
 data = os.path.join(my_path, "./sources/250518_allevents.json")
-
-#data="../sources/allevents_4.json"
-#filename="/Users/huyduc/Downloads/dhvi_calendar_code/batch_data.json"
 with open(data, 'r', encoding='utf-8') as json_file:
     data = json.load(json_file)
 app = Flask(__name__)
-events = data#[
-#    {"title": "Meeting", "start": "2025-02-05"},
-#    {"title": "Geburtstag", "start": "2025-02-12"},
-#]
-
+events = data
 @app.route('/')
 def index():
     return render_template('kalender.html')
-                           #/Users/huyduc/Downloads/dhvi_calendar_code/dash-full-calendar/assets/kalender.html')
-
 @app.route('/events')
 def get_events():
     return jsonify(events)
-
 if __name__ == '__main__':
     app.run(debug=True)
 
